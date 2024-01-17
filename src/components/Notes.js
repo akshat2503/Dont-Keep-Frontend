092e4a857e1c5700af1d27b18f123cf11f7dd54b
Akshat Gautam <ronitakki3@gmail.com>
2024-01-17 14:16:30 +0530

    Added Alert on Under Dev

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -52,7 +52,7 @@ export default function Notes(props) {
                         </div>
                         <p style={{ display: `${props.sidebarVisible ? 'block' : 'none'}` }} className='ms-2'>Notes</p>
                     </div>
-                    <div className="sidebar-item d-flex my-1 align-items-center not-active-sidebar">
+                    <div className="sidebar-item d-flex my-1 align-items-center not-active-sidebar" onClick={()=>{alert("Reminders are Under Development.")}}>
                         <div className="svgDiv ms-2">
                             <svg fill='#ffffffde'>
                                 <path d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"></path>
